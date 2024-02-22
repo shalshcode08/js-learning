25d110048deddf96b810eb47a3931c06100a2e76
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-02-22 11:07:33 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJS not browser
+
+let name = "somya"  //string data type
+let age = 20  // number data type 
+let isLoggedIn = true  //boolean data type
+
+
+// number => 2 to the power 53 is the range
+// bigint
+// string => " "
+// boolean => yes or no
+// null => stand alone value
+// undefined => declare a varible but not given the value
+// symbol => used for uniqueness 
+
+// object
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object

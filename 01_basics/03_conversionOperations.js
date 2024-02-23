fdea8c81fb6d9d0c96835adfbd4d3b17763b2139
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-02-23 06:22:22 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -16,8 +16,39 @@ console.log(typeof valueInNumber);
 let isLogged = 1
 
 let booleanIsLogged = Boolean(isLogged);
-console.log(booleanIsLogged)
+// console.log(booleanIsLogged)
 
 // 1 => true, 0 => false
 // "" => false
 // "somu" => true
+
+
+//************************ Operations *************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+//  Basic Math operations
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Somu"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(true);

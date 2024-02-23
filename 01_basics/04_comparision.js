7a34a6d746b9a8405a8b3562262edf9453f11b5f
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-02-23 06:37:45 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+
+// console.log("2">1);
+// console.log("02" > 1);
+// to compare two values we should keep in mind that the datatype should be same
+
+
+//  ===
+
+console.log("2"===2);

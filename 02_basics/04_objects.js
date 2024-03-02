18c18efa63f71e0da4c21fce457d4545e5da76e3
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-03-02 09:57:09 +0000

    More on Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,48 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "somu@gmail.com",
+    fullName : {
+        userFullName : {
+            firstName : "Somya",
+            lastName : "Shrestha"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName.lastName);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+// const obj3 = {obj1,obj2} //with this there will come the same error as arrya both will be treated as objects in the objects
+
+// const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+const User = [
+    {
+        id : 1,
+        email : "somu@gmai.com"
+    },
+    {
+        id : 2,
+        email : "shrestha@gmail.com" 
+    }
+]
+
+console.log(User[1].email);
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));

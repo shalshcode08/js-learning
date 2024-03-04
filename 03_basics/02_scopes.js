970f56dd551db0ceaa46654e4a70133d143c4a76
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-03-04 09:41:03 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,6 +10,25 @@ if(true){
     var c = 30 // but this will print and thats the problem
 }
 
+// for(let i=0; i<array.length; i++){
+//     const element = array[i];
+// }
+
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+// nested Scope 
+function one(){
+    const userName = "Somya"
+
+    function two(){
+        const website = "YouTube"
+        console.log(userName);
+    }
+
+    // console.log(website);
+
+    two();
+}
+one();
